79a04015f6ee407ff358c4180e4be596c92e1aef
Zyk <zyk@okunpedia.com>
2026-05-12 19:59:21 +0100

    odun

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,4 @@
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
 
-export default [
-  ...nextCoreWebVitals,
-  ...nextTypescript,
-];
+export default [...nextCoreWebVitals, ...nextTypescript];
